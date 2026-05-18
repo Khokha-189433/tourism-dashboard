982346c9588f74aab65fa6a01dc22b42c94d8444
Khokha <nkhokha58@gmail.com>
2026-05-18 09:38:20 +0300

    Edit Login

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./features/pages/Login";
+import Dashboard from "./features/pages/dashboard";
+import Header from "./components/layout/Header";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </div>
+  );
+}
+export default App;
+
